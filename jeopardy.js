fae0979c638d3a58c05251b22907b23359dbfee4
jcj0002 <jcj0002@gmail.com>
2018-05-22 17:18:26 -0400

    project changes

--- jeopardy.js
+++ jeopardy.js
@@ -1,10 +1,291 @@
 $(document).ready(function(){
 console.log ("connected")
 
- const genres = object {
-     description: "This lens captures details",
-     answer: "What is a macro lens",
-     points: 10
-     
- };
+    var genres10 = {
+        description: "This lens captures details...",
+        answer: "What is a macro lens",
+        points: 10
+   
+    };
+     $('#five').click(function(){
+            $(this).html(genres10.description)
+
+
+//   let timeClock = 30;
+//    let gameTimer = document.getElementById('#five')
+
+//    let timer = setInterval (countdown, 1000);
+//    function countdown(){
+//        if (timeClock === 0){
+//            clearTimeout(timeClock);
+//        } else{
+//          gameTimer.innerHTML = timeClock +'seconds remaining';
+//        timeClock--;
+//         }
+//        }
+
+   console.log('times going')
+})
+
+    
+
+ const genres20 = {
+    description: "This lens captures details",
+    answer: "What is a macro lens",
+    points: 10
+    
+
+};
+$('#six').click(function(){
+    $(this).html(genres10.description)
+
+
+
+
+
+})
+const genres30 = {
+    description: "This lens captures details",
+    answer: "What is a macro lens",
+    points: 10
+    
+};
+$('#seven').click(function(){
+    $(this).html(genres10.description)
+
+
+
+
+
+})
+const genres40 = {
+    description: "This lens captures details",
+    answer: "What is a macro lens",
+    points: 10
+    
+};
+$('#eight').click(function(){
+    $(this).html(genres10.description)
+
+})
+const genres50 = {
+    description: "This lens captures details",
+    answer: "What is a macro lens",
+    points: 10
+    
+};
+$('#nine').click(function(){
+    $(this).html(genres10.description)
+})
+const settings10 = {
+    description: "This lens captures details",
+    answer: "What is a macro lens",
+    points: 10
+    
+};
+$('#ten').click(function(){
+    $(this).html(genres10.description)
+})
+
+const settings20 = {
+    description: "This lens captures details",
+    answer: "What is a macro lens",
+    points: 10
+    
+};
+$('#eleven').click(function(){
+    $(this).html(genres10.description)
+
+
+
+
+
+})
+const settings30 = {
+    description: "This lens captures details",
+    answer: "What is a macro lens",
+    points: 10
+    
+};
+$('#twelve').click(function(){
+    $(this).html(genres10.description)
+
+
+
+
+
+})
+
+const settings40 = {
+    description: "This lens captures details",
+    answer: "What is a macro lens",
+    points: 10
+    
+};
+$('#thirteen').click(function(){
+    $(this).html(genres10.description)
+
+
+
+
+
+})
+const settings50 = {
+    description: "This lens captures details",
+    answer: "What is a macro lens",
+    points: 10
+    
+};
+$('#fourteen').click(function(){
+    $(this).html(genres10.description)
+
+
+
+
+
+})
+const equipment10 = {
+    description: "This lens captures details",
+    answer: "What is a macro lens",
+    points: 10
+    
+};
+$('#fifteen').click(function(){
+    $(this).html(genres10.description)
+
+
+
+
+
+})
+
+const equipment20 = {
+    description: "This lens captures details",
+    answer: "What is a macro lens",
+    points: 10
+    
+};
+$('#sixteen').click(function(){
+    $(this).html(genres10.description)
+
+
+
+
+
+})
+const equipment30 = {
+    description: "This lens captures details",
+    answer: "What is a macro lens",
+    points: 10
+    
+};
+$('#seventeen').click(function(){
+    $(this).html(genres10.description)
+
+
+
+
+
+})
+const equipment40 = {
+    description: "This lens captures details",
+    answer: "What is a macro lens",
+    points: 10
+    
+};
+$('#eighteen').click(function(){
+    $(this).html(genres10.description)
+
+
+
+
+
+})
+const equipment50 = {
+    description: "This lens captures details",
+    answer: "What is a macro lens",
+    points: 10
+    
+};
+$('#nineteen').click(function(){
+    $(this).html(genres10.description)
+
+
+
+
+
+})
+const lenses10 = {
+    description: "This lens captures details",
+    answer: "What is a macro lens",
+    points: 10
+    
+    
+};
+$('#twenty').click(function(){
+    $(this).html(genres10.description)
+
+
+
+
+
+})
+
+const lenses20 = {
+    description: "This lens captures details",
+    answer: "What is a macro lens",
+    points: 10
+    
+};
+$('#twentyone').click(function(){
+    $(this).html(genres10.description)
+
+
+
+
+
+})
+const lenses30 = {
+    description: "This lens captures details",
+    answer: "What is a macro lens",
+    points: 10
+    
+};
+$('#twentytwo').click(function(){
+    $(this).html(genres10.description)
+
+
+
+
+
+})
+const lenses40 = {
+    description: "This lens captures details",
+    answer: "What is a macro lens",
+    points: 10
+    
+};
+
+$('#twentythree').click(function(){
+    $(this).html(genres10.description)
+
+
+
+
+
+})
+const lenses50 = {
+    description: "This lens captures details",
+    answer: "What is a macro lens",
+    points: 10
+    
+};
+$('#twentyfour').click(function(){
+    $(this).html(genres10.description)
+
+
+
+
+
+})
 });
