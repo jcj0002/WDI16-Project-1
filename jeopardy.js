a5aaf9403004b4f124887e7eb0db2704df471ad6
jcj0002 <jcj0002@gmail.com>
2018-05-22 13:20:57 -0400

    started js

---
+++ jeopardy.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+console.log ("connected")
+
+ const genres = object {
+     description: "This lens captures details",
+     answer: "What is a macro lens",
+     points: 10
+     
+ };
+});
